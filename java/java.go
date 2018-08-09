7c9caefd1828829895a06ee289b978a9391d27cb
Rashed Abdel-Tawab <rashed@linux.com>
2018-08-09 14:08:53 -0700

    soong: Squash of lineage-sdk bringup commits
    Squash of:
    
    Author: Rashed Abdel-Tawab <rashed@linux.com>
    Date:   Thu Aug 9 14:08:53 2018 -0700
        soong: Special case Lineage SDK
         * org.lineageos.platform-res.apk needs to be installed to /system/framework
         * org.lineageos.platform-res needs to be a dependency for
           org.lineageos.platform and org.lineageos.platform.internal
         * Add other special exceptions for org.lineageos.platform-res
        Change-Id: Ic617c07c086916005ea4b88f26d31c61691a45f8
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Thu Aug 30 15:33:16 2018 -0700
        soong: make org.lineage.platform-res depend on framework-res
        *) Allows us to build org.lineage.platform-res with no_framework_libs
           true (as is done for framework-res).
        *) Whilst we're here, undo a dependency loop prevention in aar.go
           that we added during sdk bringup to allow our platform res to build
           with no_framework_libs false.
        Change-Id: Ib452a2e45112baf5d61b70b4be1ce0c01dfd84e5
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Mon Feb 4 18:56:52 2019 +0100
        Always link org.lineageos.platform-res for org.lineageos.platform.sdk
        Test: m clean && m org.lineageos.platform.sdk
        Change-Id: I58956855bd4d1157e2582103c4861e7b384b4f73
    
    Author: Sam Mortimer <sam@mortimer.me.uk>
    Date:   Fri Aug 31 10:52:29 2018 -0700
        soong: Allow framework to access lineage-sdk resources
        *) Make framework depend on lineage-sdk resource package
        *) Allows framework module to access lineage-sdk resources
           via usual org.lineageos.platform.internal.R paths.
        Change-Id: Ifd19d43d9308ac370ad40a499de16bf8ce204beb
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Tue Oct 12 17:59:21 2021 +0200
        Don't manually add lineage res to apps
        Only exception left is the hard dep of framework-res
        into org.lineage.platform-res
        Change-Id: I02585a6581da75a442b2967c89a51929d4778ea4
    
    Author: Luca Stefani <luca.stefani.ge1@gmail.com>
    Date:   Tue Oct 12 17:58:56 2021 +0200
        Make lineage res module support variation deps
        Change-Id: I823ccc87c3716bb3eb73948e228d90d45c1c29c9
    
    Change-Id: Icc18de5dfaa83fc0a1eda6f3704f3a92e1de0764

--- java/java.go
+++ java/java.go
@@ -441,6 +441,7 @@ var (
 	bootClasspathTag        = dependencyTag{name: "bootclasspath", runtimeLinked: true}
 	systemModulesTag        = dependencyTag{name: "system modules", runtimeLinked: true}
 	frameworkResTag         = dependencyTag{name: "framework-res"}
+	vendorResTag            = dependencyTag{name: "com.evervolv.platform-res"}
 	kotlinPluginTag         = dependencyTag{name: "kotlin-plugin", toolchain: true}
 	proguardRaiseTag        = dependencyTag{name: "proguard-raise"}
 	certificateTag          = dependencyTag{name: "certificate"}
@@ -507,6 +508,7 @@ type sdkDep struct {
 	java9Classpath []string
 
 	frameworkResModule string
+	vendorResModule   string
 
 	jars android.Paths
 	aidl android.OptionalPath
@@ -549,6 +551,10 @@ func sdkDeps(ctx android.BottomUpMutatorContext, sdkContext android.SdkContext, 
 	}
 	if sdkDep.systemModules != "" {
 		ctx.AddVariationDependencies(nil, systemModulesTag, sdkDep.systemModules)
+	}
+
+	if ctx.ModuleName() == "com.evervolv.platform-res" {
+		ctx.AddVariationDependencies(nil, frameworkResTag, "framework-res")
 	}
 }
 
