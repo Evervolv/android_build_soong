50361d729984a0af9e96770ca6b3c00b6422f564
LuK1337 <priv.luk@gmail.com>
2025-02-19 18:36:13 +0100

    soong: Treat vendor/lineage-priv/* as release-keys
    Change-Id: I7e7f180dda4402ada8b2124d0c26bb844dc2cb5f

--- android/config.go
+++ android/config.go
@@ -1056,6 +1056,9 @@ func (c *config) BuildKeys() string {
 	if defaultCert == "" || defaultCert == filepath.Join(testKeyDir, "testkey") {
 		return "test-keys"
 	}
+	if strings.HasPrefix(defaultCert, "vendor/ev/build/target/product/security/") {
+		return "release-keys"
+	}
 	return "dev-keys"
 }
 
