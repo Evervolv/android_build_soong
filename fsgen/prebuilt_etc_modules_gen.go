ceb3dec99f50ec04439b530ea9c351a21f3542aa
Michael Bestas <mkbestas@gmail.com>
2025-03-08 20:44:34 +0200

    Add <partition>/radio support to fsgen
    Test: m on qualcomm device with
      PRODUCT_COPY_FILES += \
        qcril.db:$(TARGET_COPY_OUT_VENDOR)/radio/qcril_database/qcril.db
    Change-Id: I2fa887eef21a8d554f27add6b63da31b53931003

--- fsgen/prebuilt_etc_modules_gen.go
+++ fsgen/prebuilt_etc_modules_gen.go
@@ -209,6 +209,7 @@ var (
 		"optee":               etc.PrebuiltOpteeFactory,
 		"overlay":             etc.PrebuiltOverlayFactory,
 		"priv-app":            etc.PrebuiltPrivAppFactory,
+		"radio":               etc.PrebuiltRadioFactory,
 		"sbin":                etc.PrebuiltSbinFactory,
 		"system":              etc.PrebuiltSystemFactory,
 		"res":                 etc.PrebuiltResFactory,
